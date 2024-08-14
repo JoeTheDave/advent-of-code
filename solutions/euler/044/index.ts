b3e5619fdd5c4b539de3f764d8fc08ed3abd7c88
Joe Davis <joethedave@gmail.com>
2024-08-13 19:25:59 -0500

    solution for euler 44

--- solutions/euler/044/index.ts
+++ solutions/euler/044/index.ts
@@ -2,7 +2,7 @@
 // https://projecteuler.net/problem=44
 
 export const displayName = 'EULER | Problem 44 | Pentagon Numbers'
-export const complete = false
+export const complete = true
 
 const pentagonalNumber = (n: number) => (n * (3 * n - 1)) / 2
 
@@ -14,24 +14,18 @@ export const solution = () => {
     pentagonalDictionary[`${n}`] = pentagonalNumber(n)
     inverseDictionary[`${pentagonalDictionary[`${n}`]}`] = n
   }
+
+  let minDiff = Infinity
   for (let k = 2; k <= maxAttemptedPentagonal; k++) {
     for (let j = 1; j < k; j++) {
       const sum = pentagonalDictionary[k] + pentagonalDictionary[j]
       const diff = pentagonalDictionary[k] - pentagonalDictionary[j]
       const sumIsP = !!inverseDictionary[sum]
       const diffIsP = !!inverseDictionary[diff]
-      if (sumIsP && diffIsP) {
-        console.log('**************************************************')
-        console.log(`P${j} = ${pentagonalDictionary[j]}`)
-        console.log(`P${k} = ${pentagonalDictionary[k]}`)
-        if (sumIsP) {
-          console.log(`P${inverseDictionary[sum]} = ${sum} | SUM IS PENTAGONAL`)
-        }
-        if (diffIsP) {
-          console.log(`P${inverseDictionary[diff]} = ${diff} | DIFF IS PENTAGONAL`)
-        }
+      if (sumIsP && diffIsP && diff < minDiff) {
+        minDiff = diff
       }
     }
   }
-  return null
+  return minDiff
 }
