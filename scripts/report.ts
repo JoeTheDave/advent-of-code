709d345365916cf2d24d6e4cf444b4b56a3332c5
Joe Davis <jdavis@grayshift.com>
2024-11-21 23:42:42 -0600

    improved aoc reporting

--- scripts/report.ts
+++ scripts/report.ts
@@ -2,14 +2,13 @@ import { existsSync, readdirSync } from 'fs'
 import _ from 'lodash'
 
 const eulerProjectPath = `${__dirname}/../solutions/euler`
+const aocProjectPath = `${__dirname}/../solutions/aoc`
 
 const generateReport = async () => {
   const args = process.argv.splice(2)
 
   if (existsSync(eulerProjectPath)) {
-    const eulerProjects = readdirSync(eulerProjectPath).map(dir =>
-      parseInt(dir),
-    )
+    const eulerProjects = readdirSync(eulerProjectPath).map(dir => parseInt(dir))
     const maxProjNum = Math.max(...eulerProjects)
     const completed = []
     const incomplete = []
@@ -50,18 +49,60 @@ const generateReport = async () => {
       error: error.length,
     }
     console.log()
-    console.log(
-      '== Project Euler =========================================================',
-    )
+    console.log('== Project Euler =========================================================')
     console.log()
     console.log(reportCounts)
     console.log()
     console.log(report)
     console.log()
-    console.log(
-      '==========================================================================',
-    )
+    console.log('==========================================================================')
     console.log()
+  }
+  if (existsSync(aocProjectPath)) {
+    console.log()
+    console.log('== Advent of Code ========================================================')
+    const currentDate = new Date()
+    _.range(2015, currentDate.getFullYear() + (currentDate.getMonth() === 11 ? 1 : 0)).forEach(year => {
+      const completion = _.fill(Array(25), 0)
+      const aocYear = `${aocProjectPath}/${year}`
+      if (existsSync(aocYear)) {
+        for (let d = 1; d <= 25; d++) {
+          const aocDay = `${aocYear}/${`${d}`.padStart(2, '0')}`
+          if (existsSync(aocDay)) {
+            const projectModulePath = `${aocDay}/index.ts`
+            const dynamicModule = require(projectModulePath)
+            completion[d - 1] = 1
+            if (dynamicModule['complete'][0]) {
+              completion[d - 1]++
+            }
+            if (dynamicModule['complete'][1]) {
+              completion[d - 1]++
+            }
+          }
+        }
+      }
+
+      const progress = completion
+        .map(v => {
+          switch (v) {
+            case 0:
+              return '   '
+            case 1:
+              return '[ ]'
+            case 2:
+              return '[-]'
+            case 3:
+              return '[*]'
+          }
+        })
+        .join('')
+
+      console.log(`    ╔══════╗`)
+      console.log(`    ║ ${year} ║`)
+      console.log(`╔═══╩══════╩════════════════════════════════════════════════════════════════╗`)
+      console.log(`║${progress}║`)
+      console.log(`╚═══════════════════════════════════════════════════════════════════════════╝`)
+    })
   }
 }
 
