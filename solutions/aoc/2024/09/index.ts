cd8210f49946831b49abf29a9eaed6de98f607f3
Joe Davis <jdavis@grayshift.com>
2024-12-10 09:31:39 -0600

    2024-9

--- solutions/aoc/2024/09/index.ts
+++ solutions/aoc/2024/09/index.ts
@@ -2,12 +2,13 @@
 // https://adventofcode.com/2024/day/9
 // https://adventofcode.com/2024/day/9/input
 
+import { findIndex, findLastIndex, fill } from 'lodash'
 import { testData, puzzleData } from './data'
 
 export const displayName = 'AOC | 2024 | Day 9 | Disk Fragmenter'
-export const complete = [false, false]
+export const complete = [true, true]
 
-const useTestData = true
+const useTestData = false
 
 const data = useTestData ? testData : puzzleData
 
@@ -19,14 +20,44 @@ export const solutionOne = () => {
       memoryBlock.push(idx % 2 === 0 ? idx / 2 : null)
     }
   })
-
-  console.log(memoryBlock.map(n => (n === null ? '.' : n)).join(''))
-  console.log(memoryBlock.indexOf(null))
-  console.log(memoryBlock.map(n => (n === null ? '.' : n)).join(''))
-
-  // return memoryBlock.map(n => (n === null ? '.' : n)).join('')
+  do {
+    let firstSpaceIdx = memoryBlock.indexOf(null)
+    const lastNumberIdx = findLastIndex(memoryBlock, a => a !== null)
+    memoryBlock[firstSpaceIdx] = memoryBlock[lastNumberIdx]
+    memoryBlock[lastNumberIdx] = null
+  } while (memoryBlock.indexOf(null) < findLastIndex(memoryBlock, a => a !== null))
+  return memoryBlock.map((val, idx) => parseInt(`${val || 0}`) * idx).reduce((sum, num) => sum + num, 0)
 }
 
 export const solutionTwo = () => {
-  return null
+  type MemoryBlock = { count: number; id: number | null }
+  const memoryBlocks: MemoryBlock[] = data
+    .split('')
+    .map((digit, idx) => ({ count: parseInt(digit), id: idx % 2 === 0 ? idx / 2 : null }))
+  const highestMemoryBlockId = Math.max(...memoryBlocks.filter(mb => mb.id !== null).map(mb => mb.id as number))
+  for (let id = highestMemoryBlockId; id >= 0; id--) {
+    const fileMemoryBlock = memoryBlocks.find(mb => mb.id === id) as MemoryBlock
+    const fileMemoryBlockIdx = memoryBlocks.indexOf(fileMemoryBlock)
+    const emptyMemoryBlockIdx = findIndex(memoryBlocks, mb => mb.id === null && mb.count >= fileMemoryBlock.count)
+    if (emptyMemoryBlockIdx > -1 && emptyMemoryBlockIdx < fileMemoryBlockIdx) {
+      const emptyMemoryBlock = memoryBlocks[emptyMemoryBlockIdx]
+      fileMemoryBlock.id = null
+      emptyMemoryBlock.id = id
+      const targetCount = emptyMemoryBlock.count
+      emptyMemoryBlock.count = fileMemoryBlock.count
+      memoryBlocks.splice(emptyMemoryBlockIdx + 1, 0, {
+        id: null,
+        count: targetCount - fileMemoryBlock.count,
+      })
+    }
+  }
+  let sum = 0
+  let idx = 0
+  memoryBlocks.forEach((memoryBlock, blockIdx) => {
+    for (let i = 0; i < memoryBlock.count; i++) {
+      sum += idx * (memoryBlock.id || 0)
+      idx++
+    }
+  })
+  return sum
 }
