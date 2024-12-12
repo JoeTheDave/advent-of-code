f5d1b7c3f6ce1ef0bdea6fb05b4eff9f05084201
Joe Davis <joethedave@gmail.com>
2024-12-12 13:44:40 -0600

    2024-12 WIP

--- solutions/aoc/2024/12/index.ts
+++ solutions/aoc/2024/12/index.ts
@@ -5,9 +5,9 @@
 import { testData, puzzleData } from './data'
 
 export const displayName = 'AOC | 2024 | Day 12 | Garden Groups'
-export const complete = [false, false]
+export const complete = [true, false]
 
-const useTestData = true
+const useTestData = false
 
 const rawData = useTestData ? testData : puzzleData
 
@@ -17,7 +17,7 @@ class Map {
 
   constructor(data: string[]) {
     this.nodes = []
-    this.nextGroupId = 0
+    this.nextGroupId = 1
     for (let y = 0; y < data.length; y++) {
       for (let x = 0; x < data[y].length; x++) {
         const id = data[y].length * y + x
@@ -28,14 +28,26 @@ class Map {
       if (node.y > 0) {
         node.n = this.nodes.find(n => n.y === node.y - 1 && n.x === node.x) as Node
       }
+      if (node.y > 0 && node.x < data[0].length - 1) {
+        node.ne = this.nodes.find(n => n.y === node.y - 1 && n.x === node.x + 1) as Node
+      }
+      if (node.x < data[0].length - 1) {
+        node.e = this.nodes.find(n => n.y === node.y && n.x === node.x + 1) as Node
+      }
+      if (node.x < data[0].length - 1 && node.y < data.length - 1) {
+        node.se = this.nodes.find(n => n.y === node.y + 1 && n.x === node.x + 1) as Node
+      }
       if (node.y < data.length - 1) {
         node.s = this.nodes.find(n => n.y === node.y + 1 && n.x === node.x) as Node
+      }
+      if (node.y < data.length - 1 && node.x > 0) {
+        node.sw = this.nodes.find(n => n.y === node.y + 1 && n.x === node.x - 1) as Node
       }
       if (node.x > 0) {
         node.w = this.nodes.find(n => n.y === node.y && n.x === node.x - 1) as Node
       }
-      if (node.x < data[0].length - 1) {
-        node.e = this.nodes.find(n => n.y === node.y && n.x === node.x + 1) as Node
+      if (node.x > 0 && node.y > 0) {
+        node.nw = this.nodes.find(n => n.y === node.y - 1 && n.x === node.x - 1) as Node
       }
     })
     while (this.nodes.some(node => !node.groupId)) {
@@ -44,6 +56,40 @@ class Map {
       this.nextGroupId++
       groupStartNode.propigateGroupId()
     }
+  }
+
+  getCost = () => {
+    let sum = 0
+    for (let i = this.nextGroupId - 1; i >= 1; i--) {
+      const groupNodes = this.nodes.filter(n => n.groupId === i)
+      const area = groupNodes.length
+      const perimeter = groupNodes
+        .map(n => {
+          return [n.n, n.s, n.e, n.w].reduce((perim, neighbor) => perim + (neighbor?.value !== n.value ? 1 : 0), 0)
+        })
+        .reduce((sum, num) => sum + num, 0)
+      sum += area * perimeter
+    }
+    return sum
+  }
+
+  getDiscountCost = () => {
+    let sum = 0
+    for (let i = this.nextGroupId - 1; i >= 1; i--) {
+      const groupNodes = this.nodes.filter(n => n.groupId === i)
+      const area = groupNodes.length
+
+      const groupPerimeterNodes = groupNodes.filter(n =>
+        [n.n, n.ne, n.e, n.se, n.s, n.sw, n.w, n.nw].some(nn => nn === null || nn.groupId !== n.groupId),
+      )
+
+      const groupPerimeterCornerNodes = groupPerimeterNodes.filter((n => ))
+
+
+
+      sum += area * groupPerimeterCornerNodes
+    }
+    return sum
   }
 }
 
@@ -54,9 +100,13 @@ class Node {
   id: number
   groupId: number | null
   n: Node | null
-  s: Node | null
+  ne: Node | null
   e: Node | null
+  se: Node | null
+  s: Node | null
+  sw: Node | null
   w: Node | null
+  nw: Node | null
 
   constructor(value: string, x: number, y: number, id: number) {
     this.value = value
@@ -65,19 +115,23 @@ class Node {
     this.id = id
     this.groupId = null
     this.n = null
-    this.s = null
+    this.ne = null
     this.e = null
+    this.se = null
+    this.s = null
+    this.sw = null
     this.w = null
+    this.nw = null
   }
 
   propigateGroupId = () => {
     if (this.groupId !== null) {
-      const neighbors = [this.n, this.s, this.e, this.w].filter(n => !!n)
+      const neighbors = [this.n, this.s, this.e, this.w].filter(
+        nn => !!nn && !nn.groupId && nn.value === this.value,
+      ) as Node[]
       neighbors.forEach(node => {
-        if (node && !node.groupId && node.value === this.value) {
-          node.value = this.value
-          node.propigateGroupId()
-        }
+        node.groupId = this.groupId
+        node.propigateGroupId()
       })
     }
   }
@@ -85,9 +139,10 @@ class Node {
 
 export const solutionOne = () => {
   const map = new Map(rawData)
-  return map
+  return map.getCost()
 }
 
 export const solutionTwo = () => {
-  return null
+  const map = new Map(rawData)
+  return map.getDiscountCost()
 }
