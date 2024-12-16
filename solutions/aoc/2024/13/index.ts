10dce181de0e44cd2e88d6238808cfb9eec5eeb7
Joe Davis <jdavis@grayshift.com>
2024-12-15 21:54:23 -0600

    2024-13

--- solutions/aoc/2024/13/index.ts
+++ solutions/aoc/2024/13/index.ts
@@ -5,15 +5,15 @@
 import { testData, puzzleData } from './data'
 
 export const displayName = 'AOC | 2024 | Day 13 | Claw Contraption'
-export const complete = [false, false]
+export const complete = [true, true]
 
-const useTestData = true
+const useTestData = false
 
 const rawData = useTestData ? testData : puzzleData
 
 type Coordinates = {
-  x: number
-  y: number
+  x: bigint
+  y: bigint
 }
 
 type MachineInfo = {
@@ -26,22 +26,44 @@ const compileMachineData = (data: string[]) => {
   const machines: MachineInfo[] = []
   for (let i = 0; i < data.length; i += 4) {
     const machine = { a: {}, b: {}, prize: {} } as MachineInfo
-    const buttonA = data[i].match(/X\+(\d+), Y\+(\d+)/)
-
-    // const buttonA = data[i].replace('Button A: ', '').replace('X+', '').replace('Y+', '').split(', ')
-    const buttonB = data[i + 1].replace('Button B: ', '').replace('X+', '').replace('Y+', '').split(', ')
-    const prize = data[i + 2].replace('Prize: ', '')
-    console.log(buttonA)
-    console.log(buttonB)
-    console.log(prize)
+    const buttonA = data[i].match(/X\+(\d+), Y\+(\d+)/) as RegExpMatchArray
+    const buttonB = data[i + 1].match(/X\+(\d+), Y\+(\d+)/) as RegExpMatchArray
+    const prize = data[i + 2].match(/X\=(\d+), Y\=(\d+)/) as RegExpMatchArray
+    machine.a.x = BigInt(parseInt(buttonA[1]))
+    machine.a.y = BigInt(parseInt(buttonA[2]))
+    machine.b.x = BigInt(parseInt(buttonB[1]))
+    machine.b.y = BigInt(parseInt(buttonB[2]))
+    machine.prize.x = BigInt(parseInt(prize[1]))
+    machine.prize.y = BigInt(parseInt(prize[2]))
+    machines.push(machine)
   }
+  return machines
 }
 
 export const solutionOne = () => {
-  // const machines: MachineInfo[] = compileMachineData(rawData)
-  // return machines
+  const machines: MachineInfo[] = compileMachineData(rawData)
+  return machines
+    .map(m => {
+      const a = (m.prize.x * m.b.y - m.prize.y * m.b.x) / (m.a.x * m.b.y - m.a.y * m.b.x)
+      const aWhole = (m.prize.x * m.b.y - m.prize.y * m.b.x) % (m.a.x * m.b.y - m.a.y * m.b.x) === 0n
+      const b = (m.prize.x - m.a.x * a) / m.b.x
+      const bWhole = (m.prize.x - m.a.x * a) % m.b.x === 0n
+      return aWhole && bWhole ? 3n * a + b : 0n
+    })
+    .reduce((sum, num) => BigInt(sum) + BigInt(num), 0n)
 }
 
 export const solutionTwo = () => {
-  return null
+  const machines: MachineInfo[] = compileMachineData(rawData)
+  return machines
+    .map(m => {
+      m.prize.x += BigInt(10000000000000)
+      m.prize.y += BigInt(10000000000000)
+      const a = (m.prize.x * m.b.y - m.prize.y * m.b.x) / (m.a.x * m.b.y - m.a.y * m.b.x)
+      const aWhole = (m.prize.x * m.b.y - m.prize.y * m.b.x) % (m.a.x * m.b.y - m.a.y * m.b.x) === 0n
+      const b = (m.prize.x - m.a.x * a) / m.b.x
+      const bWhole = (m.prize.x - m.a.x * a) % m.b.x === 0n
+      return aWhole && bWhole ? 3n * a + b : 0n
+    })
+    .reduce((sum, num) => BigInt(sum) + BigInt(num), 0n)
 }
