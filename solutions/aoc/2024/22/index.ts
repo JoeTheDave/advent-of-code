73fc2aee98b4c67caa005ef1f1b27da88d290b23
Joe Davis <jdavis@grayshift.com>
2024-12-23 16:23:48 -0600

    2024-22 WIP

---
+++ solutions/aoc/2024/22/index.ts
@@ -0,0 +1,20 @@
+// Advent of Code | 2024 | Day 22 | Monkey Market
+// https://adventofcode.com/2024/day/22
+// https://adventofcode.com/2024/day/22/input
+
+import { testData, puzzleData } from './data'
+
+export const displayName = 'AOC | 2024 | Day 22 | Monkey Market'
+export const complete = [false, false]
+
+const useTestData = true
+
+const data = useTestData ? testData : puzzleData
+
+export const solutionOne = () => {
+  return null
+}
+
+export const solutionTwo = () => {
+  return null
+}
